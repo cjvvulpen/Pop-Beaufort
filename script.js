8715ea57f2b203c86eec68a3ae0f27d138bbee29
Chelsea van Vulpen <92001502+cjvvulpen@users.noreply.github.com>
2024-11-22 20:00:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Optional: Add smooth scroll effect when clicking navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
 
